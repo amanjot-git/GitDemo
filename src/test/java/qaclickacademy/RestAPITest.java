a0a91a8bfb4570d58ec3224729f579e8dfded732
Aman <navdll91@gmail.com>
2020-01-30 13:23:12 -0500

    This is commit from Asian Time Zone guy

--- src/test/java/qaclickacademy/RestAPITest.java
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -8,6 +8,8 @@ public class RestAPITest {
 	public void postJira()
 	{
 		System.out.println("PostJira");
+		System.out.println("PostJira2");
+		System.out.println("PostJira3");
 	}
 	@Test
 	public void deleteTwitter()
